620f9f52433baf4f18e5f852a635c286acf62e3b
Unknown <thanos_ro@hotmail.com>
2018-07-12 19:45:01 -0700

    upd on opt problem

--- peak_opt.m
+++ peak_opt.m
@@ -1,38 +1,35 @@
-% Load_dem = load demand vector
-% No_gen_units = number of generation units Ns
-% Supply_vec = supply vector of each gen. unit (gamma) (matrix Ns*O*T)
-% Ns rows, O columnts T height
-% Cost_fun = quadratic function of energy generation Cost_fun =
-% a*Supply_vec^2; a = 0.15 $/Kwh
-% d upper & lower bound of discharge rate of MESS
-% T total time intervals
-% x binary vector; shows the operational state of each gen. unit
-% O number of operational states of each generation unit
-%
-
-%     s_cap = 600;
-%     Cost_vec = a.*Supply_vec;
-% No_gen_units = Ns
-% Time_slots = T
-% Op_States = O
-% Supply_vec = gamma_3d
-% cost_en_3d = a*Supply_vec_3d.^2;
-cost_en_3d = a*Supply_vec_3d;
-cvx_begin quiet
+%% variables
+% peak and normal price
+p_elec = 0.05;
+p_peak = 12/30;
+% Energy capacity kWh
+E_cap = 500;
+% Power Capacity kW
+P_max = 100;
+% Time resolution is the length of the load array 
+Time_slots = size(Lt_day,1);
+% Depth of Discharge DoD
+DoD = 0.9;
+alpha = (1-DoD)/2;
+%% optimization problem 
+% cost_en_3d = a*Supply_vec_3d;
+cvx_begin %quiet
 cvx_solver gurobi
-cvx_solver_settings('TIMELIMIT',10); 
-variable x_3d(No_gen_units,Op_States,Time_slots-1) binary 
-variable s_t_mat_3d(1,1,Time_slots-1)
-minimize sum(sum(sum(cost_en_3d.*x_3d))) + p*(sum(s_t_mat_3d))%^2)
+% cvx_solver_settings('TIMELIMIT',10); 
+variable b_ch(Time_slots) 
+variable b_dc(Time_slots) 
+variable b(Time_slots)
+minimize p_elec*sum(Lt_day(Time_slots)-b(Time_slots)) + p_peak*max(Lt_day(Time_slots)-b(Time_slots))
+minimize p_elec*sum(total_energy_demand(Time_slots)-b(Time_slots)) + p_peak*
 subject to 
     % const. #2
-    Load_dem <= sum(sum(Supply_vec_3d(:,:,:).*x_3d(:,:,:))) + s_t_mat_3d;
+    b(Time_slots) == b_dc(Time_slots) - b_ch(Time_slots) ;
     % const. #3
-    sum(x_3d,2) == 1;
+    alpha*E_cap <= cumsum() <= (1-a)*E_cap
     % const. #4
-    0<= cumsum(s_t_mat_3d) <= s_cap;
+    0<=  b_ch(:) <= P_max;
     % const. #5 
-   0 <= s_t_mat_3d(:) <= d;
+    0<=  b_dc(:) <= P_max;
 cvx_end
-min_cost_S = sum(sum(sum(cost_en_3d.*x_3d))) + p*(sum(s_t_mat_3d));%^2);
+% min_cost_S = sum(sum(sum(cost_en_3d.*x_3d))) + p*(sum(s_t_mat_3d));%^2);
 % end
