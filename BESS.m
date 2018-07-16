8d733b8b27989bed742afc605db23eb98f0027e5
Unknown <thanos_ro@hotmail.com>
2018-07-16 16:58:07 -0700

    finalized peak shave opt

---
+++ BESS.m
@@ -0,0 +1,23 @@
+%% Samsung https://zerohomebills.com/wp-content/uploads/Samsung_ESS_Data_Sheet.pdf
+% Energy capacity kWh
+E_cap = 3.6;
+% Power Capacity kW
+P_max = 2;
+% Depth of Discharge DoD
+DoD = 0.9;
+%% Canadian Solar https://www.canadiansolar.com/fileadmin/user_upload/downloads/datasheets/v5.3/Canadian_Solar-Datasheet-Camel_ESS_v5.3C2_en.pdf
+% Energy capacity kWh
+E_cap = 7.2;
+% Power Capacity kW
+P_max = 4.5;
+% Depth of Discharge DoD
+DoD = 0.9;
+%% E-gear  http://www.e-gear.us/images/brochures/E-Gear%20BESS%20Datasheet-160428.pdf
+
+%% BMZ http://www.prosolar.net/userfiles/BMZ_ESS7%200_Germany.pdf
+% Energy capacity kWh
+E_cap = 6.74;
+% Power Capacity kW
+P_max = 8;
+% Depth of Discharge DoD
+DoD = 0.9;
--- peak_opt.m
+++ peak_opt.m
@@ -1,28 +1,32 @@
 fig_count = 0;
 %% variables
 % peak and normal price
-p_elec = 0.05;
+p_base = 0.05;
 p_peak = 12/30;
+assert(p_base<p_peak,'Peak price lower that base')
 % Energy capacity MWh
 E_cap = 500;
 % energy initial storage
-E_init = 100;
+E_init = 50;
 % Power Capacity MW
-P_max = 120;
+P_max = 250;
+% Depth of Discharge DoD
+DoD = 1;
 % Time resolution is the length of the load array 
 Time_slots = size(Lt_day,1);
 % peak charge threshold in kW
-L_thres = 500;
+L_thres = 700;
 L_thres_ar = L_thres*ones(size(Lt_day));
 % Depth of Discharge DoD
 DoD = 0.9;
 alpha = (1-DoD)/2;
 fig_count = fig_count + 1;
 %%
-micro_grid_index = 5;
-Lt_day = 1000*monthly_norm_data.Sep(1:24,micro_grid_index);
-plot(Lt_day)
-%% optimization problem 
+micro_grid_index = 6;
+Lt_day = 1000*monthly_norm_data.Sep(4*24+1:5*24,micro_grid_index);
+% plot(Lt_day)
+fig_count = fig_count + 1;
+% optimization problem 
 % cost_en_3d = a*Supply_vec_3d;
 cvx_begin %quiet
 cvx_solver gurobi
@@ -34,7 +38,8 @@ variable max_var%(Time_slots)
 % variable per_shave
 % minimize p_elec*sum(Lt_day(:)-b(:)) + p_peak*max_var%max([(Lt_day(:)-b(:)-L_thres_ar(:))])
 minimize p_elec*sum(Lt_day(:)-b(:)) + p_peak*max_var%max([(Lt_day(:)-b(:)-L_thres_ar(:))])
-
+%  minimize sum(p_base*min(Lt_day(:)-b(:), L_thres_ar(:)) + p_peak*max(Lt_day(:)-b(:)-L_thres_ar(:),0) )
+% minimize sum(p_base*(Lt_day(:)-b(:)) + (p_peak-p_base)*max(Lt_day(:)-b(:)-L_thres_ar(:),0) )
 subject to 
     % const. #2
     b(:) == b_dc(:) - b_ch(:);
@@ -47,11 +52,11 @@ subject to
     % const. #5 
     0 <=  b_dc(:) <= P_max;
     % --- max constraint--- % 
-    max_var >= Lt_day(:)-b(:) - L_thres_ar(:);
+    max_var >= Lt_day(:)-b(:); %- L_thres_ar(:);
     max_var >= 0;
 %     max_var == max(Lt_day(:)-b(:)-L_thres_ar(:))
     % const # 6
-     Lt_day(:)-b(:) >= 0
+%      Lt_day(:)-b(:) >= 0
 %    Lt_day(:)-b(:) <= L_thres_ar(:)
 cvx_end
 %%
@@ -60,7 +65,7 @@ figure(2343+fig_count)%+p_i+1)
    title(['P_{max} = ',num2str(P_max),' E_{cap}= ',num2str(E_cap)])
    hold on;
    % plot straight line to denote the peak consumption
-   plot(500*ones(1,length(Lt_day)),'--')
+   plot(L_thres*ones(1,length(Lt_day)),'--')
    hold on
    plot(Lt_day-b)
    set(gca, 'yGrid','on')
@@ -82,11 +87,15 @@ plot(b,'b')
    set(gca, 'yGrid','on')
 %    title('Micro-grid daily consumption')
    ylabel('Consumption (kW)')
-   xlim([0 96])
-   xticks(0:12:96)
-   xticklabels(0:3:24)
+%    xlim([0 96])
+%    xticks(0:12:96)
+%    xticklabels(0:3:24)
    xlabel('Time (hours)')
 %    legend('Load','Threshold','Shaved','Location','Northwest')
+%% energy price
+base_cost = sum(p_base*Lt_day(:) + (p_peak-p_base)*m700ax(Lt_day(:)-L_thres_ar(:),0) );
+disp(['Base cost is ',num2str(base_cost)])
+sum(p_base*min(Lt_day(:), L_thres_ar(:)) + p_peak*max(Lt_day(:)-L_thres_ar(:),0))
 %%
 cvx_begin %quiet
 cvx_solver gurobi
