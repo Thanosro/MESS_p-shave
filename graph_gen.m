9b823975f0e716cef5b5c20387e82fc163f2cc37
Unknown <thanos_ro@hotmail.com>
2018-07-16 23:57:25 -0700

    found mult edges indices

--- graph_gen.m
+++ graph_gen.m
@@ -4,7 +4,7 @@ clc; clear all; close all;
 %m , d # of days
 mg = 3; days =4;
 % # of MESS types
-MESS_TYPES = 5;
+NO_MESS_TYPES = 2;
 base_reloc_cost = 5;
 reloc_mat =tril(ones(mg));
 
@@ -27,18 +27,23 @@ G0 = addedge(G0,Ed_S0);
 G0 = addedge(G0,Ed_T0);
 %%
 % reshape array to find the add nodes indices
-resh_1 = reshape(1:mg*days*2,mg,[])
-add_edge_ind = sort(reshape(resh_1',2,[])');
-add_edge_ind_rep = repelem(add_edge_ind,MESS_TYPES,1);
+resh_1 = reshape(1:mg*days*2,mg,[]);
+% resh_1 = repmat(resh_1,NO_MESS_TYPES,1)
+% add_edge_ind = sort(reshape(resh_1',2,[])');
+add_edge_ind = reshape(resh_1',2,[])';
+add_edge_ind_rep = repelem(add_edge_ind,NO_MESS_TYPES,1);
 add_edge_table = table(add_edge_ind_rep,[ones(length(add_edge_ind_rep),1)],...
     'VariableNames',{'EndNodes','Weight'});
-G0 = addedge(G0,add_edge_table)
+G0 = addedge(G0,add_edge_table);
 %% add edges costs and capacities 
 % edge capacities are 1 except those of S* and T* with capacity equal to
 % the # of MESS
 % assign label nodes
 G0.Edges.Labels = (1:numedges(G0))';
 G0.Edges.Capacities = ones(numedges(G0),1);
+%% the index for each edge for each MESS type is
+% MESS_NO = 1,2,3 for MESS number 1, 2, etc
+(0:days-1)*(mg*days+mg*NO_MESS_TYPES) + 1%MESS_NO
 %%
 figure(500)
 % fullscreen figure
@@ -46,7 +51,10 @@ figure(500)
 h1 =plot(G0);
 layout(h1,'layered','Direction','right','Sources', 'S*','Sinks','T*')
 highlight(h1,'Edges',1:numedges(G0),'LineWidth',1.5)
+% highlight(h1,'Edges',findedge(G0,add_edge_ind(:,1),add_edge_ind(:,2)),'EdgeColor','r','LineWidth',1.5)
 highlight(h1,1:numnodes(G0),'MarkerSize',4,'NodeColor','r')
+% test hightlight edges
+highlight(h1,'Edges',ans,'EdgeColor','r','LineWidth',1.5)
 %------------ commands to make the figure pretty------- 
 % xd = get(h1, 'XData');
 % yd = get(h1, 'YData');
